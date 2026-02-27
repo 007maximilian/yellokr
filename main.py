a6783b5e7f73ed390940f0ce09bf1ad427cd8006
007maximilian <max.mazu007@gmail.com>
2026-02-27 06:48:37 +0300

    This is commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+import random
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+class CircleDrawer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.circles = []
+        self.drawButton.clicked.connect(self.add_circle)
+        
+    def add_circle(self):
+        diameter = random.randint(20, 100)
+        x = random.randint(0, self.width() - diameter)
+        y = random.randint(0, self.height() - diameter)
+        self.circles.append((x, y, diameter))
+        self.update()
+    
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setBrush(QColor(255, 255, 0))
+        for x, y, diameter in self.circles:
+            painter.drawEllipse(x, y, diameter, diameter)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = CircleDrawer()
+    window.show()
+    sys.exit(app.exec_())
